deab6c78f8b077374c9783aa0eabe7c0b6b13dba
Jesper Engberg <jeengbe@gmail.com>
2024-02-17 15:29:02 +0100

    Start page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'File Share: Seamless & Secure Peer-to-Peer Sharing Platform',
+  description:
+    "File Share offers a cutting-edge solution for peer-to-peer file transfers. Embrace the ease of sharing files directly between devices without intermediaries, ensuring your data's security with robust encryption protocols. Whether for work or leisure, File Share supports all file types and sizes, facilitating limitless sharing possibilities. Get started with File Share today for fast, secure, and straightforward file exchanges.",
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html lang='de'>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { GitHubIcon } from '@/components/ui/github-icon';
 import Link from 'next/link';
 
 // @ts-expect-error -- Polyfill
@@ -9,13 +11,55 @@ Symbol.asyncDispose ??= Symbol('Symbol.asyncDispose');
 
 export default function Page() {
   return (
-    <div>
-      <div>
-        <h1>P2P File Sharing</h1>
+    <div className='flex flex-col h-screen justify-center items-center gap-32'>
+      <div className='flex flex-col gap-2 justify-center items-center flex-1'>
+        <div className='flex flex-col gap-12 shadow-sm rounded-lg p-16 py-12 pb-14 border bg-card text-card-foreground'>
+          <div className='flex flex-col space-y-1.5 items-center'>
+            <div className='text-2xl font-semibold leading-none tracking-tight'>
+              P2P File Sharing
+            </div>
+            <div className='text-sm text-muted-foreground'>
+              Share Files Easily and Securely
+            </div>
+          </div>
+          <div className='flex flex-col gap-4 items-center pt-0'>
+            <Button className='w-full' asChild>
+              <Link href='/share'>Send</Link>
+            </Button>
+            <Button className='w-full' asChild>
+              <Link href='/receive'>Receive</Link>
+            </Button>
+          </div>
+        </div>
+        <div className='text-muted-foreground underline underline-offset-2 hover:text-blue-500 transition-colors flex'>
+          <Link href='/about'>How it works -&gt;</Link>
+        </div>
       </div>
-      <div>
-        <Link href='/send'>Send</Link>
-        <Link href='/receive'>Receive</Link>
+      <div className='flex justify-center w-full gap-2 pb-4'>
+        <Link
+          className='text-secondary-800 hover:text-blue-500 transition-colors flex items-center gap-1'
+          target='_blank'
+          href='https://github.com/jeengbe/fileshare'
+        >
+          <span className='inline-block text-secondary-500 size-4'>
+            <GitHubIcon />
+          </span>
+          GitHub
+        </Link>
+        <span className='text-secondary-500'>&#x2022;</span>
+        <Link
+          className='text-secondary-800 hover:text-blue-500 transition-colors'
+          href='/privacy'
+        >
+          Privacy Policy
+        </Link>
+        <span className='text-secondary-500'>&#x2022;</span>
+        <Link
+          className='text-secondary-800 hover:text-blue-500 transition-colors'
+          href='/legal'
+        >
+          Legal Notice
+        </Link>
       </div>
     </div>
   );
