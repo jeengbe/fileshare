9afc83d2daa9994a0a88b3e4b1d352330bcd3a99
Jesper Engberg <jeengbe@gmail.com>
2024-02-18 12:49:31 +0100

    own mitm

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -1,5 +1,7 @@
 import streamSaver from 'streamsaver';
 
+streamSaver.mitm = './ss-mitm.html';
+
 export type FileDownloader = (info: {
   fileId: string;
   filename: string;
@@ -7,12 +9,41 @@ export type FileDownloader = (info: {
   stream: ReadableStream<Uint8Array>;
 }) => Promise<void>;
 
+let downloads = 0;
+const beforeUnload = (event: BeforeUnloadEvent) => {
+  event.preventDefault();
+
+  return 'There are downloads in progress. They will be canceled if you leave the page.';
+};
+
 export function useDownload(): FileDownloader | null {
   return async ({ stream, filename, size }) => {
     const writable = streamSaver.createWriteStream(filename, {
       size,
     }) as WritableStream<Uint8Array>;
 
-    await stream.pipeTo(writable);
+    const abort = () => {
+      void writable.abort();
+    };
+
+    const donePromise = stream.pipeTo(writable);
+
+    window.addEventListener('unload', abort);
+
+    downloads++;
+
+    if (downloads === 1) {
+      window.addEventListener('beforeunload', beforeUnload);
+    }
+
+    await donePromise;
+
+    downloads--;
+
+    if (!downloads) {
+      window.removeEventListener('beforeunload', beforeUnload);
+    }
+
+    window.removeEventListener('unload', abort);
   };
 }
